f4ed59981550afc91689c755c70603593a8ff6a9
aary <aaryadevv8@gmail.com>
2026-03-22 20:37:00 +1100

    Add configuration file with constants and settings

---
+++ src/config.js
@@ -0,0 +1,42 @@
+export const ORG = 'TwilightSMP';
+
+export const COLORS = {
+  push: 0x9b59b6,
+  pull_request: 0xe74c3c,
+  issues: 0xf1c40f,
+  release: 0x1abc9c,
+  heartbeat: 0x2c2f33,
+};
+
+export const SMP_ROLES = ['Relic', 'Cursed', 'Dead', 'Wanderer', 'Bound', 'Forsaken'];
+
+export const SMP_FLAVOR = [
+  'the realm stirs',
+  'a seal has broken',
+  'the void speaks',
+  'twilight descends',
+  'an echo remains',
+  'the chronicle updates',
+  'fate is rewritten',
+  'a shadow moves',
+];
+
+export const GITHUB_API = 'https://api.github.com';
+
+export const FETCH_LIMITS = {
+  commits: 5,
+  pull_requests: 5,
+  issues: 5,
+  releases: 3,
+};
+
+export const STATE_DIR = '../state';
+
+export const STATE_FILES = {
+  push: 'push.json',
+  pull_request: 'pr.json',
+  issues: 'issues.json',
+  release: 'release.json',
+};
+
+export const HEARTBEAT_ENABLED = true;
