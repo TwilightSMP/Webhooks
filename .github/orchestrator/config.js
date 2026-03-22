103e905da534a33335a57334a3dba20eb3e794a0
aary <aaryadevv8@gmail.com>
2026-03-22 20:19:30 +1100

    Add configuration for colors and roles

---
+++ .github/orchestrator/config.js
@@ -0,0 +1,14 @@
+export const config = {
+  colors: {
+    push: 0x9b59b6,
+    pr: 0xe74c3c,
+    issues: 0xf1c40f,
+    release: 0x1abc9c
+  },
+  roles: {
+    relic: 'Relic Holder',
+    cursed: 'Cursed',
+    dead: 'Dead'
+  },
+  repo: process.env.GITHUB_ORG
+};
